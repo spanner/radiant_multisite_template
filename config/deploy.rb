f96e38c3dc73b9fcfae472c29947ed924d096edd
William Ross <will@spanner.org>
2011-10-30 10:51:04 +0000

    clean out for v1

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ after "deploy:update" do
   run "ln -s #{shared_path}/public/robots.txt #{current_release}/public/robots.txt"
   run "ln -s #{shared_path}/public/images/local #{current_release}/public/images/local"
   run "ln -s #{shared_path}/public/fonts #{current_release}/public/fonts"
-  run "ln -s /var/www/radiant_beta #{current_release}/vendor/radiant"
+  run "ln -s /var/www/radiant_edge #{current_release}/vendor/radiant"
 end
 
 namespace :deploy do
